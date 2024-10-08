83dd13813b74ac88928f21c4b986be62c880a1da
Yuwen Zhang <yuwen01@gmail.com>
2024-10-08 12:28:53 -0700

    clean script more

--- example/script/src/main.rs
+++ example/script/src/main.rs
@@ -1,15 +1,9 @@
-use clap::Parser;
 use groth16_solana::{verify_proof_fixture, SP1ProofFixture, GROTH16_VK_BYTES};
-use sp1_sdk::{utils, ProverClient, SP1ProofWithPublicValues, SP1Stdin};
-use std::str::FromStr;
-use strum_macros::{Display, EnumIter, EnumString};
+use sp1_sdk::{utils, SP1ProofWithPublicValues};
 
 fn main() {
     // Setup logging for the application.
     utils::setup_logger();
-
-    // Parse command line arguments.
-    let args = Cli::parse();
 
     // Where to save / load the proof from.
     let proof_file = "../proofs/fibonacci_proof.bin";
@@ -28,18 +22,15 @@ fn main() {
 #[cfg(test)]
 mod tests {
     use super::*;
-    use strum::IntoEnumIterator;
 
     #[test]
     fn test_programs() {
-        Elf::iter().for_each(|program| {
-            // Read the serialized fixture from the file.
-            let fixture_file = format!("../proof-fixtures/{}_fixture.bin", program.to_string());
-            let fixture = SP1ProofFixture::load(&fixture_file).unwrap();
+        // Read the serialized fixture from the file.
+        let fixture_file = "../proof-fixtures/fibonacci_fixture.bin";
+        let fixture = SP1ProofFixture::load(&fixture_file).unwrap();
 
-            // Verify the proof.
-            let result = verify_proof_fixture(&fixture, GROTH16_VK_BYTES);
-            assert!(result.is_ok(), "Proof verification failed for {}", program);
-        });
+        // Verify the proof.
+        let result = verify_proof_fixture(&fixture, GROTH16_VK_BYTES);
+        assert!(result.is_ok(), "Proof verification failed for fibonacci");
     }
 }
