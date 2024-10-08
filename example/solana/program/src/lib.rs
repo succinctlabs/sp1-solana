2826af55be8d10f5e458b578e6134cbae170a8d9
Yuwen Zhang <yuwen01@gmail.com>
2024-10-08 11:18:55 -0700

    add comments, rename test file

--- example/solana/program/src/lib.rs
+++ example/solana/program/src/lib.rs
@@ -17,8 +17,13 @@ pub fn process_instruction(
     _accounts: &[AccountInfo],
     instruction_data: &[u8],
 ) -> ProgramResult {
+    // Deserialize the fixture from the instruction data.
     let fixture = SP1ProofFixture::try_from_slice(instruction_data).unwrap();
+
+    // Get the SP1 Groth16 verification key from the `groth16-solana` crate.
     let vk = groth16_solana::GROTH16_VK_BYTES;
+
+    // Verify the proof.
     let result = verify_proof_fixture(&fixture, &vk);
     msg!("Result: {:?}", result);
     Ok(())
