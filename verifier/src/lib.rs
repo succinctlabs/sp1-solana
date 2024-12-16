ce999e224d141c1a2297d0fda0b6b266b5bd4e24
Yuwen Zhang <yuwen01@gmail.com>
2024-12-16 11:38:52 -0800

    nit

--- verifier/src/lib.rs
+++ verifier/src/lib.rs
@@ -62,15 +62,9 @@ pub fn verify_proof_raw(proof: &[u8], public_inputs: &[u8], vk: &[u8]) -> Result
         &public_inputs.inputs,
         &vk,
     )
-    .map_err(|err| {
-        println!("error internal: {:?}", err);
-        Error::VerificationError
-    })?;
+    .map_err(|_| Error::VerificationError)?;
 
-    if verifier.verify().map_err(|err| {
-        println!("error internal2: {:?}", err);
-        Error::VerificationError
-    })? {
+    if verifier.verify().map_err(|_| Error::VerificationError)? {
         println!("Verification successful.");
         Ok(())
     } else {
